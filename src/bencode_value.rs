a8e7194a5e32f7453044de08a9a67c9a3252896b
AusAgentSmith <AusAgentSmith@sprooty.com>
2026-04-06 19:29:20 +1000

    Fix tests: include torrent fixture, fix relative paths

--- src/bencode_value.rs
+++ src/bencode_value.rs
@@ -187,7 +187,7 @@ mod tests {
     #[test]
     fn test_deserialize_torrent_dyn() {
         let mut buf = Vec::new();
-        let filename = "../librtbit/resources/ubuntu-21.04-desktop-amd64.iso.torrent";
+        let filename = "resources/ubuntu-21.04-desktop-amd64.iso.torrent";
         std::fs::File::open(filename)
             .unwrap()
             .read_to_end(&mut buf)
@@ -202,7 +202,7 @@ mod tests {
     #[test]
     fn test_serialize_torrent_dyn() {
         let mut buf = Vec::new();
-        let filename = "../librtbit/resources/ubuntu-21.04-desktop-amd64.iso.torrent";
+        let filename = "resources/ubuntu-21.04-desktop-amd64.iso.torrent";
         std::fs::File::open(filename)
             .unwrap()
             .read_to_end(&mut buf)
